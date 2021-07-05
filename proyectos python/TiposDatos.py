5cf29dc561dce7eff681d63189f9176bb0029bf5
Ronaldo Arias Parada <56136740+ariasRonaldo25@users.noreply.github.com>
2021-07-05 11:15:30 -0500

    Update TiposDatos.py
    ajustando archivo.

--- proyectos python/TiposDatos.py
+++ proyectos python/TiposDatos.py
@@ -1,3 +1,13 @@
-x = False
+x: bool = False
+print(x)
+print(type(x))
+x= 10
+print(x)
+#muestra el tipo de dato al cual hace referencia una variable
+print(type(x))
+x=10.0
+print(x)
+print(type(x))
+x="Ronaldo"
 print(x)
 print(type(x))
