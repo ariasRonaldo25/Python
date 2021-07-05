ffba480eb05a09d58c5b50039abad3bd79d320d1
Ronaldo Arias <ronaldorafaelap@ufps.edu.co>
2021-07-05 16:53:58 -0500

    trabajando con operadores aritmeticos y ejercicio area y perimetro rectangulo

---
+++ proyectos python/OperadoresAritmeticos.py
@@ -0,0 +1,18 @@
+operandoA = 3
+operandoB = 2
+suma = operandoA + operandoB
+#print('Resultado suma:', suma)
+print(f'Resultado suma: {suma} ')
+resta = operandoA - operandoB
+print(f'Resultado resta: {resta} ')
+multiplicacion = operandoA * operandoB
+print(f'Resultado multiplicación: {multiplicacion}')
+division = operandoA / operandoB
+print(f'Resultado división: {division}')
+#division que retorne el valor entero
+division = operandoA // operandoB
+print(f'Resultado división (int): {division}')
+modulo = operandoA % operandoB
+print(f'Resultado residuo división (módulo): {modulo}')
+exponente = operandoA ** operandoB
+print(f'Resultado exponente: {exponente}')
