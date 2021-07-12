4d665db9e0b1237d4c06bcf54fb1714fdde23548
Ronaldo Arias <ronaldorafaelap@ufps.edu.co>
2021-07-12 16:28:37 -0500

    ejercicio numero mayor

---
+++ proyectos python/numeroMayor.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Proporciona el Número 1:"))
+numero2 = int(input("Proporciona el Número 2:"))
+
+if(numero1 > numero2):
+    print("Número 1 es mayor")
+else:
+    print("Número 2 es mayor")
