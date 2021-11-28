2cafd70dea9b839eee7bdd2ff5f5f4532be6990c
Ronaldo Arias Parada <ronaldorafaelap@ufps.edu.co>
2021-11-28 12:33:48 -0500

    integrando el modelo a la vista aplicando el parametro mvt

--- django/personas_django/crud/webapp/views.py
+++ django/personas_django/crud/webapp/views.py
@@ -2,11 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from personas.models import Persona
+
 
 def bienvenido(request):
+    no_personas=Persona.objects.count()
+    return render(request, 'Bienvenido.html', {'no_personas': no_personas})
 
-    mensajes={ 'msj1': 'valor mensaje1', 'msj2': 'valor mensaje2' }
-    return render(request,'Bienvenido.html',mensajes)
+
+#mensajes={ 'msj1': 'valor mensaje1', 'msj2': 'valor mensaje2' }
+#return render(request,'Bienvenido.html',mensajes)
 
 #def despedirse(request):
  #   return HttpResponse('Adiós mundo desde django')
