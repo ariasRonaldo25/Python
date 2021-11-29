72b9f9b1e031de59b11a4eb9f3988dff6f6c41ae
Ronaldo Arias Parada <ronaldorafaelap@ufps.edu.co>
2021-11-28 21:07:50 -0500

    metodo agregar al bd

--- django/personas_django/crud/crud/urls.py
+++ django/personas_django/crud/crud/urls.py
@@ -18,18 +18,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import path
 
-from personas.views import detallePersonas
+from personas.views import detallePersonas, nuevaPersona
 from webapp.views import bienvenido
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', bienvenido),
+    path('', bienvenido, name='index'),
     #path('/despedirse', despedirse)
-    path('detalle_persona/<int:id>', detallePersonas)
-
-
-
+    path('detalle_persona/<int:id>', detallePersonas),
+    path('nueva_persona', nuevaPersona)
 
 ]
 
